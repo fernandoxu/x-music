1ed8640708988f4fe872a3423e1e83a7aa3e2643
fernandoxu <fernandoxu9@gmail.com>
2022-07-03 00:57:51 +0800

    🐛 middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  staticPageGenerationTimeout: 80000,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
