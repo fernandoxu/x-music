2e526d83106bf285242e86fd9d283efb715f7c36
fernandoxu <fernandoxu9@gmail.com>
2022-07-03 00:31:59 +0800

    🐛 type

--- lib/store.ts
+++ lib/store.ts
@@ -2,7 +2,7 @@ import create from 'zustand';
 import { Song, Artist } from '@prisma/client';
 
 export interface SongState extends Song {
-  artist: Artist;
+  artist: Partial<Artist>;
 }
 
 interface SongsState {
